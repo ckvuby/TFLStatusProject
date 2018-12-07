06a5630c75052f7f682afc7f78b84091b7307ffd
Naziya <nazzkadri786@gmail.com>
2018-12-07 15:54:42 +0000

    Refactoring, removed unnecessary code from the tests

--- TFLStatusLibrary.Tests/TFLApiClientShould.cs
+++ TFLStatusLibrary.Tests/TFLApiClientShould.cs
@@ -51,66 +51,13 @@ namespace TFLStatusLibrary.Tests
         {
             Mock<IHttpClient> httpClient = new Mock<IHttpClient>();
             var url = "https://api.tfl.gov.uk/line/mode/tube/status?detail=true";
-
-            // Create json
-
-            var responseAsJson = new JArray
-            {
-                new JObject
-                {
-                    new JProperty("id", "Bakerloo"),
-                    new JProperty("name", "Bakerloo"),
-                    new JProperty("lineStatuses", new JArray
-                    {
-                        new JObject
-                        {
-                            new JProperty("statusSeverityDescription", "Good Service"),
-                            new JProperty("reason", "")
-                        }                    
-                    })        
-                }
-            };
-
-            var responseAsJsonTwo = new JArray
-            {
-                new JObject
-                {
-                    new JProperty("id", "Circle"),
-                    new JProperty("name", "Circle"),
-                    new JProperty("lineStatuses", new JArray
-                    {
-                        new JObject
-                        {
-                            new JProperty("statusSeverityDescription", "Minor Delays"),
-                            new JProperty("reason", "Circle Line: MINOR DELAYS due to emergency work in the Euston Square area.")
-                        }
-                    })
-                }
-            };
-
-            // Pass json as string
             var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                //Content = new StringContent(JsonConvert.SerializeObject(responseAsJson))
               // Content = new StringContent("[{\"$type\":\"Tfl.Api.Presentation.Entities.Line, Tfl.Api.Presentation.Entities\",\"id\":\"circle\",\"name\":\"Circle\",\"modeName\":\"tube\",\"disruptions\":[],\"created\":\"2018-11-28T11:37:03.677Z\",\"modified\":\"2018-11-28T11:37:03.677Z\",\"lineStatuses\":[{\"$type\":\"Tfl.Api.Presentation.Entities.LineStatus, Tfl.Api.Presentation.Entities\",\"id\":0,\"lineId\":\"circle\",\"statusSeverity\":9,\"statusSeverityDescription\":\"Minor Delays\",\"reason\":\"Circle Line: MINOR DELAYS due to emergency work in the Euston Square area. \",\"created\":\"0001-01-01T00:00:00\",\"validityPeriods\":[{\"$type\":\"Tfl.Api.Presentation.Entities.ValidityPeriod, Tfl.Api.Presentation.Entities\",\"fromDate\":\"2018-12-07T11:01:48Z\",\"toDate\":\"2018-12-08T01:29:00Z\",\"isNow\":true}],\"disruption\":{\"$type\":\"Tfl.Api.Presentation.Entities.Disruption, Tfl.Api.Presentation.Entities\",\"category\":\"RealTime\",\"categoryDescription\":\"RealTime\",\"description\":\"Circle Line: MINOR DELAYS due to emergency work in the Euston Square area. \",\"affectedRoutes\":[{\"$type\":\"Tfl.Api.Presentation.Entities.RouteSection, Tfl.Api.Presentation.Entities\",\"id\":\"1633\",\"name\":\"Edgware Road (Circle Line) Underground Station - Hammersmith (H&C Line) Underground Station\",\"direction\":\"inbound\",\"originationName\":\"Edgware Road (Circle Line) Underground Station\",\"destinationName\":\"Hammersmith (H&C Line) Underground Station\",\"routeSectionNaptanEntrySequence\":[]}]")
                Content = new StringContent("[{\"$type\":\"Tfl.Api.Presentation.Entities.Line, Tfl.Api.Presentation.Entities\",\"id\":\"bakerloo\",\"name\":\"Bakerloo\",\"modeName\":\"tube\",\"disruptions\":[],\"created\":\"2018-11-28T11:37:03.687Z\",\"modified\":\"2018-11-28T11:37:03.687Z\",\"lineStatuses\":[{\"$type\":\"Tfl.Api.Presentation.Entities.LineStatus, Tfl.Api.Presentation.Entities\",\"id\":0,\"statusSeverity\":10,\"statusSeverityDescription\":\"Good Service\",\"created\":\"0001-01-01T00:00:00\",\"validityPeriods\":[]}],\"routeSections\":[],\"serviceTypes\":[{\"$type\":\"Tfl.Api.Presentation.Entities.LineServiceTypeInfo, Tfl.Api.Presentation.Entities\",\"name\":\"Regular\",\"uri\":\"/Line/Route?ids=Bakerloo&serviceTypes=Regular\"}],\"crowding\":{\"$type\":\"Tfl.Api.Presentation.Entities.Crowding, Tfl.Api.Presentation.Entities\"}}]")
             };
 
             httpClient.Setup(x => x.GetAsync(url)).Returns(Task.FromResult<HttpResponseMessage>(responseMessage));
-
-            // Expect List of LineInformation
-            /*
-            var expectedLineInformation = new List<LineInformation>
-            {
-                new LineInformation
-                {
-                    lineId = "circle",
-                    lineName = "Circle",
-                    lineStatus = "Minor Delays",
-                    statusReason = "Circle Line: MINOR DELAYS due to emergency work in the Euston Square area."
-                }
-            };
-               */
 
 
             var expectedLineInformation = new List<LineInformation>
@@ -124,21 +71,20 @@ namespace TFLStatusLibrary.Tests
              }
          };
 
-
-
             TFLApiClient tflClient = new TFLApiClient(httpClient.Object);
           var response = tflClient.SetupAndMakeApiCallAndReturnFormattedData().ToList();
 
           // Assert the passed json resolves to expected line information
           Assert.Equal(expectedLineInformation[0].lineId, response[0].lineId);
-          //Assert.Equal(expectedLineInformation, response.ToList());
+            Assert.Equal(expectedLineInformation[0].lineName, response[0].lineName);
+            Assert.Equal(expectedLineInformation[0].lineStatus, response[0].lineStatus);
+            Assert.Equal(expectedLineInformation[0].statusReason, response[0].statusReason);
+            //Assert.Equal(expectedLineInformation, response.ToList());
 
-      }
+        }
 
 
-    
-
-      [Fact]
+        [Fact]
       public async Task WriteErrorMessageToConsoleIfBadStatusCode()
       {
           using (StringWriter sw = new StringWriter())
