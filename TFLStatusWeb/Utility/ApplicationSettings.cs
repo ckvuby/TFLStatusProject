39b9d432269a78bcf446d8993b38ab9a91bfde53
Naziya <nazzkadri786@gmail.com>
2018-12-12 12:43:36 +0000

    Added instances to IOC in startup, updated controller.

---
+++ TFLStatusWeb/Utility/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFLStatusWeb.Utility
+{
+    public class ApplicationSettings
+    {
+        public static string WebApiUrl { get; set; }
+    }
+}
