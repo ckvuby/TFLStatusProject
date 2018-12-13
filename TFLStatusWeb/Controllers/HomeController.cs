d0cd709b8bc23b2e294387d79c3af56feff1163a
ChloeHelen <cmingay@fastmail.fm>
2018-12-13 14:56:20 +0000

    Two branches merged

--- TFLStatusWeb/Controllers/HomeController.cs
+++ TFLStatusWeb/Controllers/HomeController.cs
@@ -25,6 +25,7 @@ namespace TFLStatusWeb.Controllers
         public IActionResult Index()
         {
             var lineInformationData = tflApiClient.SetupAndMakeApiCallAndReturnFormattedData();
+            HttpContext.Response.Headers.Add("refresh", "300; url=" + Url.Action("Index"));
             return View(lineInformationData);
         }
 
