2208b751937d0e4ce461903d9dd9a3771bba14a8
Naziya <nazzkadri786@gmail.com>
2018-12-17 11:44:31 +0000

    removed unnecessary using statements and spaces.

--- TFLWebController.Tests/TflWebControllerShould.cs
+++ TFLWebController.Tests/TflWebControllerShould.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Reflection;
-using System.Threading.Tasks;
 using TFLStatusLibrary;
 using TFLStatusWeb.Controllers;
 using Xunit;
@@ -15,11 +10,9 @@ namespace TFLWebController.Tests
 {
     public class TflWebControllerShould
     {
-     
         [Fact]
         public void TReturn_IndexView_With_TubeLines_ListWithServiceStatus()
         {
-
             var tflApiClient = new Mock<ITFLAPIClient>();
             var expectedLineInformation = new List<LineInformation>
          {
@@ -32,17 +25,12 @@ namespace TFLWebController.Tests
              }
          };
             tflApiClient.Setup(x => x.SetupAndMakeApiCallAndReturnFormattedData()).Returns(expectedLineInformation);
-        
-
             HomeController homeController = new HomeController(tflApiClient.Object);
             IActionResult result = homeController.Index();
-
             var viewResult = Assert.IsType<ViewResult>(result);
-
             Assert.NotNull(result);
             var model = Assert.IsAssignableFrom<IEnumerable<LineInformation>>(viewResult.ViewData.Model);
             Assert.Equal(expectedLineInformation[0].lineId, model.ElementAt(0).lineId);
-
         }
     }
 }
